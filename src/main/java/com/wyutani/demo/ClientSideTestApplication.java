fcf85de7ed4c95e1845c944df62aec43896c9127
dacevedom <d.acevedom1993@gmail.com>
2025-11-12 11:59:14 -0300

    First iteration of Client Side App development Test using Springboot + Bootstrap + Maven

---
+++ src/main/java/com/wyutani/demo/ClientSideTestApplication.java
@@ -0,0 +1,13 @@
+package com.wyutani.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSideTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSideTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyutani/demo/ClientSideTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyutani.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSideTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
