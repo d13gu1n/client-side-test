a6e61a9da54949f43d1b8bd83303bbb2784d1944
dacevedom <d.acevedom1993@gmail.com>
2025-11-13 11:08:04 -0300

    Created stream endpoint on Client Side Test app

--- src/main/java/com/wyutani/demo/ClientSideTestApplication.java
+++ src/main/java/com/wyutani/demo/ClientSideTestApplication.java
@@ -1,13 +1,50 @@
 package com.wyutani.demo;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
+@RestController
 public class ClientSideTestApplication {
+	
+	@GetMapping("/user")
+	public String user() {
+		return "Dieguin";
+	}
 
+	@PostMapping("/greet")
+	public String greet(@ModelAttribute Greeting values) {
+		return "Hello " + values.getValue() + "!";
+	}
+
+	@GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> stream() {
+		return Flux.interval(Duration.ofSeconds(5)).map(
+			value -> value + ":" + System.currentTimeMillis()
+		);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientSideTestApplication.class, args);
+	}
+
+	static class Greeting {
+		private String value;
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
 	}
 
 }
